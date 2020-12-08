10a27893359b30dc0e40c73c7b7dda7ed05d90b0
Storline <69596121+Storline@users.noreply.github.com>
2020-12-08 14:11:00 +0300

    Update RomanNumeral.java

--- src/Solutions/RomanNumeral.java
+++ src/Solutions/RomanNumeral.java
@@ -1,4 +1,5 @@
 package Solutions;
+
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
@@ -45,5 +46,28 @@ enum RomanNumeral {
         }
 
         return result;
+    }
+
+    public static String arabicToRoman(int number) {
+        if ((number <= 0) || (number > 4000)) {
+            throw new IllegalArgumentException(number + " is not in range (0,4000]");
+        }
+
+        List<RomanNumeral> romanNumerals = RomanNumeral.getReverseSortedValues();
+
+        int i = 0;
+        StringBuilder sb = new StringBuilder();
+
+        while ((number > 0) && (i < romanNumerals.size())) {
+            RomanNumeral currentSymbol = romanNumerals.get(i);
+            if (currentSymbol.getValue() <= number) {
+                sb.append(currentSymbol.name());
+                number -= currentSymbol.getValue();
+            } else {
+                i++;
+            }
+        }
+
+        return sb.toString();
     }
 }
