6b371da25823cfa2578a30ec80fb4a07ee290dc6
Storline <69596121+Storline@users.noreply.github.com>
2020-12-10 11:25:47 +0300

    Update RomanNumeral.java

--- src/Solutions/RomanNumeral.java
+++ src/Solutions/RomanNumeral.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 enum RomanNumeral {
-    I(1), IV(4), V(5), IX(9), X(10), XL(40), L(50), XL(90);
+    I(1), IV(4), V(5), IX(9), X(10), XL(40), L(50), XC(90);
 
     private final int value;
 
